8a809f6c5f7f91ee4774278ca6abe7981d1dcfac
Perzan <PerzanDevelopment@gmail.com>
2020-12-21 16:51:48 -0500

    Add 'requests>=2.25,<3' to installation requirements

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ distutils.core.setup(
     version="1.0.0",
     author="Perzan",
     author_email="PerzanDevelopment@gmail.com",
-    url="https://github.com/Perzan/univoucher"
+    url="https://github.com/Perzan/univoucher",
+    install_requires=[
+        "requests>=2.25,<3"
+    ]
 )

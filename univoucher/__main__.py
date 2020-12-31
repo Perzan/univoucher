13f2a71b2c445fc9534af432d8cb2d81c62aed08
Perzan <PerzanDevelopment@gmail.com>
2020-12-31 11:58:12 -0500

    Rename function

--- univoucher/__main__.py
+++ univoucher/__main__.py
@@ -61,7 +61,7 @@ vman = VoucherManager(host=args.host, username=args.username, password=password,
 
 response:Response = vman.create(duration=args.duration, amount=args.amount, uses=args.uses)
 
-def json_vouchers():
+def vouchers_to_json():
     return [voucher.dictify() for voucher in response.content]
 
 #####################################
@@ -71,6 +71,6 @@ if args.output:
         exit(ERR_OUTPUT_IS_DIR["code"])
     
     with open(args.output, "w") as stream:
-        dump(obj=json_vouchers(), fp=stream, indent=4, sort_keys=True)
+        dump(obj=vouchers_to_json(), fp=stream, indent=4, sort_keys=True)
 else:
-    print(dumps(obj=json_vouchers(), indent=4, sort_keys=True))
+    print(dumps(obj=vouchers_to_json(), indent=4, sort_keys=True))
